44bfe673bd7bb39f753a8660991ec7bba34d819d
Staffan Olsson <staffan@repos.se>
2017-11-07 15:24:01 +0100

    At what level is kafka.admin meant to integrate with? Command line and plain text?

---
+++ src/main/java/se/yolean/kafka/kubernetes/health/Metrics.java
@@ -0,0 +1,14 @@
+package se.yolean.kafka.kubernetes.health;
+
+import kafka.utils.ZkUtils;
+
+public class Metrics {
+
+	public static void main(String[] args) {
+		// https://github.com/Yolean/kafka-topic-client/blob/master/src/main/java/Client.java#L28
+		
+		// https://github.com/apache/kafka/blob/1.0/core/src/main/scala/kafka/admin/TopicCommand.scala#L56
+		
+	}
+
+}
